202afc0f48bff1751254ad0d6771a80692de7d27
LingGrani <luthfiangger2510@gmail.com>
2024-12-19 08:55:25 +0700

    Create Custom Color

--- app/src/main/java/com/example/ucp2/ui/theme/Color.kt
+++ app/src/main/java/com/example/ucp2/ui/theme/Color.kt
@@ -1,11 +1,13 @@
 package com.example.ucp2.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val biruMain = Color(0xFF273D7C)
+val biruMainLight = Color(0xFF3F63C9)
+val mainRight = Color(0xFF514689)
+val mainRightLight = Color(0xFF5A53C9)
+val mainLeft = Color(0xFF468988)
+val mainLeftLight = Color(0xFF53B4C9)
+//Gradient
+val mainGradient = Brush.horizontalGradient(listOf(biruMain, biruMainLight))
