2a4be648c8da58563974f9955b513d92c24abd2e
LingGrani <luthfiangger2510@gmail.com>
2024-12-19 19:06:36 +0700

    Implement dual Theme

--- app/src/main/java/com/example/ucp2/MainActivity.kt
+++ app/src/main/java/com/example/ucp2/MainActivity.kt
@@ -3,28 +3,30 @@ package com.example.ucp2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.ucp2.ui.navigation.PengelolaHalaman
 import com.example.ucp2.ui.theme.UCP2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            UCP2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            var isDarkTheme by remember { mutableStateOf(false) }
+            UCP2Theme (
+                darkTheme = isDarkTheme
+            ) {
+                PengelolaHalaman(
+                    isDarkTheme = isDarkTheme,
+                    onThemeChange = { isDarkTheme = it },
+                    check = isDarkTheme.toString()
+                )
             }
         }
     }
--- app/src/main/java/com/example/ucp2/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/PengelolaHalaman.kt
@@ -1,4 +1,24 @@
 package com.example.ucp2.ui.navigation
 
-class PengelolaHalaman {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.ucp2.R
+import com.example.ucp2.ui.view.MainView
+import com.example.ucp2.ui.view.dosen.HomeDosenView
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier,
+    isDarkTheme: Boolean,
+    onThemeChange: (Boolean) -> Unit,
+    check: String
+){
+
 }
