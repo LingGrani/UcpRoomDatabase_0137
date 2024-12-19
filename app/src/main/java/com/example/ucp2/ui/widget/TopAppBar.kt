b982d36f881001f0ebae5868f7610e8383bab73b
LingGrani <luthfiangger2510@gmail.com>
2024-12-19 19:03:38 +0700

    Update TopAppBar

--- app/src/main/java/com/example/ucp2/ui/widget/TopAppBar.kt
+++ app/src/main/java/com/example/ucp2/ui/widget/TopAppBar.kt
@@ -1,16 +1,22 @@
 package com.example.ucp2.ui.widget
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -19,35 +25,42 @@ fun TopAppBar(
     onBack: () -> Unit,
     showBackButton: Boolean = true,
     judul: String,
-    modifier: Modifier = Modifier
 ) {
-    Box(
+    Row (
         modifier = Modifier
             .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
             .padding(16.dp),
-        contentAlignment = Alignment.Center // Pastikan konten di tengah
-    ) {
-        if (showBackButton) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                TextButton(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Box (
+            modifier = Modifier.weight(0.1f)
+        ){
+            if (showBackButton) {
+                IconButton(
                     onClick = onBack,
-                    modifier = Modifier.align(Alignment.CenterVertically)
                 ) {
-                    Text("Kembali")
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "",
+                        modifier = Modifier,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
                 }
-                Spacer(modifier = Modifier.weight(2f))
             }
         }
-
-        // Teks judul
-        Text(
-            text = judul,
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(Alignment.Center)
-        )
+        Box (
+            modifier = Modifier.weight(0.8f),
+            contentAlignment = Alignment.Center
+        ){
+            Text(
+                text = judul,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
     }
 }
